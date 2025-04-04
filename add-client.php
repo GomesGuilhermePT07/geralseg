5bf6679b145abb3a418960ae31d0d3c9c5e8c391
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-04-04 10:25:53 +0100

    add

--- add-client.php
+++ add-client.php
@@ -12,14 +12,14 @@
 </head>
 <body>
     <p class="add-client-db">Adicionar cliente à base de dados</p>
-    <form action="./assets/php/captar_cliente" class="inputs-form">
+    <form action="./assets/php/captar_cliente.php" class="inputs-form">
         <input type="text" id="cliente" name="cliente" class="inputs" placeholder="Cliente" required>
         <input type="text" id="ref" name="ref" class="inputs" placeholder="Referência" required>
         <input type="text" id="nome" name="nome" class="inputs" placeholder="Nome" required>
         <input type="number" id="proposta" name="proposta" class="inputs" placeholder="Valor da proposta" required>
         <input type="text" id="sistema" name="sistema" class="inputs" placeholder="Sistema" required>
 
-        <button type="submit">Adicionar cliente</button>
+        <button type="submit" class="add-btn">Adicionar cliente</button>
     </form>
 </body>
 </html>
