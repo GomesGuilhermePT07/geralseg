907f990b13368e6554b7b1aec620b4ac61831c96
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-04-04 10:13:14 +0100

    add

--- add-client.php
+++ add-client.php
@@ -14,23 +14,14 @@
     <form action="./assets/php/captar_cliente">
         <label for="nome">Cliente:</label>
         <input type="text" id="cliente" name="cliente" class="inputs" required>
-        <br>
-
         <label for="nome">Referência:</label>
         <input type="text" id="ref" name="ref" class="inputs" required>
-        <br>
-
         <label for="nome">Nome:</label>
         <input type="text" id="nome" name="nome" class="inputs" required>
-        <br>
-
         <label for="nome">Valor da proposta:</label>
         <input type="number" id="proposta" name="proposta" class="inputs" required>
-        <br>
-        
         <label for="nome">Sistema:</label>
         <input type="text" id="sistema" name="sistema" class="inputs" required>
-        <br>
     </form>
 </body>
 </html>
