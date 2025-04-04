79ae73ccceff8dd4f17f93c4f60cabbabd690ed7
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-04-04 09:49:08 +0100

    form test

---
+++ add-client.php
@@ -0,0 +1,19 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-PT">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/style.css">
+    <title>GG | Adicionar Cliente</title>
+</head>
+<body>
+    <form action="./assets/php/captar_cliente">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required>
+    </form>
+</body>
+</html>
