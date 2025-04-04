fa66920af65876ab0f2d9d491bb11acabb43e890
GomesGuilhermePT07 <gomesguilherme.pt@gmail.com>
2025-04-04 11:21:52 +0100

    add

--- assets/php/config.php
+++ assets/php/config.php
@@ -7,7 +7,7 @@ $dbname = "geralseg";
 
 try{
     $pdo = new pdo("mysql:host=$host;port=3306;dbname=$dbname", $username, $password);
-    echo "Conexão bem sucedida";
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
